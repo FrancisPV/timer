e88d7cb1d9f4ea74f637e3164eb7914020884922
Françis <francispellerinvenne@hotmail.fr>
2020-06-30 18:09:39 +0000

    Timer that make a sound (or not here cause it's not working) every parameter(seconde)

--- timer1.js
+++ timer1.js
@@ -2,6 +2,12 @@ const argumentList = process.argv
 const parameters = argumentList.slice(2);
 
 for (let arg = 0; arg < parameters.length; arg++) {
+  if (parameters[arg] < 0) {
+    continue;
+  }
+  if (isNaN(parseFloat(parameters[arg]))) {
+    continue;
+  }
   setTimeout(() => {
     process.stdout.write('Imagine a sound (ding)' + '\n');
   }, parameters[arg] * 1000)
